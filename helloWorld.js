2a2c46cdc17c85142dede2188f6857e62206e3a7
ted <linyc1983@gmail>
2016-12-02 11:41:22 +0800

    add readme.md

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (req,res) {
+
+	
+
+	// console.log(req.url);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World!');
+	// res.end('Hello World!');
+	fs.readFile(__dirname + '/about.html', function (err, data) {
+		if(err){
+			console.log(err);
+		}else{
+			res.end(data);	
+		}
+		
+	});
+
 }).listen(3000);
 
 console.log('Server started on localhost:3000; press Ctrl-C to terminate...');
