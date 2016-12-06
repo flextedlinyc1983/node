42d4a66c2ef280143d0e7a82a166e0a8d69787a0
ted <linyc1983@gmail>
2016-12-06 13:10:32 +0800

    add mssql node_module

--- meadowlark.js
+++ meadowlark.js
@@ -2,6 +2,77 @@ var express = require('express');
 var fs = require('fs');
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 
+
+
+var sql = require('mssql');
+ 
+var config = {
+    user: 'sa',
+    password: '',
+    server: 'localhost', // You can use 'localhost\\instance' to connect to named instance 
+    database: 'flaps',
+    stream: true, // You can enable streaming globally 
+ 
+    // options: {
+    //     encrypt: true // Use this if you're on Windows Azure 
+    // }
+}
+
+
+
+var mssql_demo_query = function (query_str) {
+
+	
+
+	sql.connect(config, function(err) {
+	    // ... error checks 
+	 
+	    var request = new sql.Request();
+	    request.stream = true; // You can set streaming differently for each request 
+	    // request.query('select * from aa'); // or request.execute(procedure); 
+	    request.query(query_str); // or request.execute(procedure); 
+	 
+	    request.on('recordset', function(columns) {
+	    	// Emitted once for each recordset in a query 
+	    	// console.log("columns: " + columns);
+	    	console.log("name: " + columns.aa.name);
+	
+	    });
+	 
+	    request.on('row', function(row) {
+	    	// Emitted for each row in a recordset 
+	    	console.log("value: " + row.aa);
+	    });
+	 
+	    request.on('error', function(err) {
+	    	// May be emitted multiple times 
+	    	console.log("err: " + err);
+	    });
+	 
+	    request.on('done', function(affected) {
+	    	// Always emitted as the last one 
+	    	console.log("affected: " + affected);
+	    });
+	});
+	 
+	sql.on('error', function(err) {
+	    // ... error handler 
+	    console.log('err: ' + err);
+	});	
+
+	
+}
+
+ 
+
+
+
+var mssql_demo = {query: mssql_demo_query };
+console.log(mssql_demo.query('select * from aa'))
+
+
+
+
 var app = express();
 
 app.engine('handlebars', handlebars.engine);
