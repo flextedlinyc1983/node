ecc53ea26b93b49becab70e819945903e259431c
ted <linyc1983@gmail>
2017-01-17 17:59:23 +0800

    add method

--- public/js/main.js
+++ public/js/main.js
@@ -9,4 +9,69 @@ require.config({
 
 require(["math"], function(math) {
   console.log(math.add(1,1));  
+  math.stateManager.fly();
+
+
+	var topics = {};	 
+	jQuery.Topic = function( id ) {
+	    var callbacks,
+	        topic = id && topics[ id ];
+	    if ( !topic ) {
+	        callbacks = jQuery.Callbacks();
+	        topic = {
+	            publish: callbacks.fire,
+	            subscribe: callbacks.add,
+	            unsubscribe: callbacks.remove
+	        };
+	        if ( id ) {
+	            topics[ id ] = topic;
+	        }
+	    }
+	    return topic;
+	};
+	var fn1 = function(val){
+		console.log('fn1 ' + val )
+	}
+	var fn2 = function(val){
+		console.log('fn2 ' + val )
+	}
+	$.Topic( "mailArrived" ).subscribe( fn1 );
+	$.Topic( "mailArrived" ).subscribe( fn2 );
+	$.Topic( "mailArrived" ).publish( "hello world!" );
+
+
+	$(document).ready(function () {
+
+
+		$.fn.tabs = function (control) {
+			var el = $(this);
+			control = $(control);
+
+			el.on('click','li',function () {
+				var tabName = $(this).attr("data-tab");
+				el.trigger("change.tabs",tabName);
+			})
+
+			el.on("change.tabs",function (e,tabName) {
+				el.find('li').removeClass("active");
+				el.find(">[data-tab='" + tabName + "']").addClass('active');
+			})
+			el.on("change.tabs",function (e,tabName) {
+				control.find('>[data-tab]').removeClass("active");
+				control.find(">[data-tab='" + tabName + "']").addClass('active');
+			})
+
+
+			var firstName = el.find('li:first').attr('data-tab');
+			el.trigger("change.tabs",firstName);
+
+			return this;
+		}
+
+		$("ul#tabs").tabs("#tabsContent");
+
+	})
+
+
+
 });
--- public/js/require_test.js
+++ public/js/require_test.js
@@ -2,7 +2,29 @@ define(function (){
 	var add = function (x,y){
     	return x+y;
     };
-    return {
-    	add: add
+    
+
+    var stateManager = {
+ 
+	  fly: function () {
+	 
+	    var self = this;
+	 
+	    $( "#container" )
+	          .unbind()
+	          .on( "click", "div.toggle", function ( e ) {
+	            self.handleClick( $(e.target) );
+	          });
+	  },
+	 
+	  handleClick: function ( elem ) {
+		console.log('handleClick')
+	    elem.find( "span" ).toggle( "slow" );
+	  }
+	};
+
+	return {
+    	add: add,
+    	stateManager: stateManager
     };
 });
